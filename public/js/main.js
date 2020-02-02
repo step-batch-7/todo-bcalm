14609d2e99c3815f6ca900b4c35126ba42d62289
Vikram <58028680+bcalm@users.noreply.github.com>
2020-02-02 19:07:10 +0530

    Added: task name on the screen.

--- public/js/main.js
+++ public/js/main.js
@@ -14,14 +14,33 @@ const closeSideBar = function(e) {
 };
 
 const showAddListOption = function(e) {
-  console.log('hello');
   document.getElementById('addList').style['display'] = 'block';
+  document.getElementById('sideBar').style.width = '0vw';
+  document.getElementById('menuBar').style['display'] = 'block';
 };
 
 const cancelList = function(e) {
-  console.log('cancel');
+  document.getElementById('addList').style['display'] = 'block';
+};
+
+const generateToDoList = task => {
+  return task.map(c => c.name).join('\n');
 };
 
 const saveList = function(e) {
-  console.log('save');
+  document.getElementById('addList').style['display'] = 'none';
+  const userTask = document.getElementById('taskName');
+  const task = {name: userTask.value};
+  userTask.value = '';
+  const req = new XMLHttpRequest();
+  req.onload = function() {
+    if (this.status === 200) {
+      document.getElementById('todoList').innerText = generateToDoList(
+        JSON.parse(this.responseText)
+      );
+    }
+  };
+  req.open('POST', 'http://localhost:8080/');
+  req.setRequestHeader('content-type', 'application/json');
+  req.send(JSON.stringify(task));
 };
