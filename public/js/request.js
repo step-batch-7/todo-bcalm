67d09454aa256e2272ee9ca96d4c657c3b9d3451
Vikram <58028680+bcalm@users.noreply.github.com>
2020-02-03 21:33:58 +0530

    Added: file request.js
    Extraced request function and put them on request.js

---
+++ public/js/request.js
@@ -0,0 +1,40 @@
+const saveTodo = function() {
+  document.getElementById('addList').style['display'] = 'none';
+  const userTask = document.getElementById('taskName');
+  const req = new XMLHttpRequest();
+  req.onload = function() {
+    const correctStatusCode = 200;
+    if (this.status === correctStatusCode) {
+      createTask(JSON.parse(this.responseText));
+    }
+  };
+  req.open('POST', 'http://localhost:8080/addTodo');
+  req.setRequestHeader('content-type', 'application/json');
+  req.send(JSON.stringify(userTask.value));
+  userTask.value = '';
+};
+
+const homePage = function() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'http://localhost:8080/getAllTodo');
+  req.send();
+  req.onload = function() {
+    const correctStatusCode = 200;
+    if (this.status === correctStatusCode) {
+      createTask(JSON.parse(this.responseText));
+    }
+  };
+};
+
+const deleteTodo = function() {
+  const id = JSON.parse(event.target.id);
+  const req = new XMLHttpRequest();
+  req.open('DELETE', 'http://localhost:8080/deleteTodo');
+  req.send(JSON.stringify(id));
+  req.onload = function() {
+    const correctStatusCode = 200;
+    if (this.status === correctStatusCode) {
+      createTask(JSON.parse(this.responseText));
+    }
+  };
+};
