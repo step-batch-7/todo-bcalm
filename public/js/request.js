6cc3111fb0683bbd8db33b1040789069ff2d7f80
Vikram <58028680+bcalm@users.noreply.github.com>
2020-02-05 13:38:05 +0530

    Refactored: logic for adding a new todo

--- public/js/request.js
+++ public/js/request.js
@@ -1,9 +1,4 @@
-const sendHttpRequest = function(req, sendData, action, id) {
-  const actions = {
-    addTask: createTaskName,
-    addTodo: createTodo
-  };
-  const callback = actions[action];
+const sendHttpRequest = function(req, sendData, callback, id) {
   const xhr = new XMLHttpRequest();
   xhr.onload = function() {
     const correctStatusCode = 200;
@@ -13,25 +8,25 @@ const sendHttpRequest = function(req, sendData, action, id) {
   };
   xhr.open(req.method, req.url);
   xhr.setRequestHeader('content-type', 'application/json');
-  xhr.send(sendData);
+  xhr.send(JSON.stringify(sendData));
 };
 
 const saveTodo = function() {
   document.getElementById('addList').style['display'] = 'none';
   const todo = document.getElementById('todoName');
-  const sendData = JSON.stringify(todo.value);
+  const sendData = todo.value;
   const req = {method: 'POST', url: '/addTodo'};
-  sendHttpRequest(req, sendData, 'addTodo');
+  sendHttpRequest(req, sendData, createToDoBlock);
   todo.value = '';
 };
 
 const addTask = function() {
   const {id, value} = event.target;
   if (event.key === 'Enter' && value.trim()) {
-    const data = JSON.stringify({title: value, id});
+    const data = {title: value, id};
     event.target.value = '';
     const req = {method: 'POST', url: '/addTask'};
-    sendHttpRequest(req, data, 'addTask', id);
+    sendHttpRequest(req, data, createTaskName, id);
   }
 };
 
@@ -53,19 +48,19 @@ const addExistedTodo = function() {
 const deleteTodo = function() {
   const sendData = event.target.parentNode.parentNode.parentNode.id;
   const req = {method: 'DELETE', url: '/deleteTodo'};
-  sendHttpRequest(req, sendData, 'addTodo');
+  sendHttpRequest(req, sendData, createTodo);
 };
 
 const deleteTask = function() {
   const taskId = event.target.parentNode.parentNode.id;
   const [id] = taskId.split('_');
   const req = {method: 'DELETE', url: '/deleteTask'};
-  sendHttpRequest(req, taskId, 'addTask', id);
+  sendHttpRequest(req, taskId, createTaskName, id);
 };
 
 const toggleStatus = function(event) {
   const taskId = event.target.parentNode.parentNode.id;
   const [id] = taskId.split('_');
   const req = {method: 'POST', url: '/toggleStatus'};
-  sendHttpRequest(req, sendData, 'addTask', id);
+  sendHttpRequest(req, sendData, createTaskName, id);
 };
--- public/js/todo.js
+++ public/js/todo.js
@@ -48,4 +48,3 @@ const createTodo = function(todoList) {
   }
   todoList.forEach(createToDoBlock);
 };
-
