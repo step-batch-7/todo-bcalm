0108c8a3c8618e1ca2fd6918a4b367f31d525e1f
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-02-08 10:12:02 +0530

    Removed: todo.htm and todoFile

--- public/TODO_files/main.js
+++
@@ -1,8 +0,0 @@
-const showAddListOption = function() {
-  document.getElementById('addList').style['transform'] = 'scale(1)';
-};
-
-const cancelList = function() {
-  document.getElementById('listName').value = '';
-  document.getElementById('addList').style['transform'] = 'scale(0)';
-};
