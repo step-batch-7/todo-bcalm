941ee495ca2030a2b1a90c06f0dd7e6e94cd5cfe
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-02-08 13:59:10 +0530

    Added search by task feature:
    matched task will be highlighted

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,52 @@
+const select = document.querySelectorAll.bind(document);
+const getElement = document.getElementById.bind(document);
+
 const showAddListOption = function() {
-  document.getElementById('addList').style['transform'] = 'scale(1)';
+  getElement('addList').style['transform'] = 'scale(1)';
 };
 
 const cancelList = function() {
-  document.getElementById('listName').value = '';
-  document.getElementById('addList').style['transform'] = 'scale(0)';
+  getElement('listName').value = '';
+  getElement('addList').style['transform'] = 'scale(0)';
 };
 
-const filterTodo = function(matchList, todo, text) {
-  const todoTitle = todo.innerText;
-  if (todoTitle.includes(text)) {
-    matchList.push(todo.parentElement.parentElement.parentElement.id);
-  }
-  return matchList;
-};
-
-const getAllTodo = () => Array.from(document.querySelectorAll('.todo span'));
-
-const showResult = function(todo, text) {
+const showMatchedTodo = function(todo, text) {
   const todoTitle = todo.innerText;
   const todoId = todo.parentElement.parentElement.parentElement.id;
   if (todoTitle.includes(text)) {
-    document.getElementById(todoId).style.display = 'block';
+    getElement(todoId).style.display = 'block';
   } else {
-    document.getElementById(todoId).style.display = 'none';
+    getElement(todoId).style.display = 'none';
   }
 };
 
 const searchByTodo = function() {
   const text = event.target.value;
-  const allTodo = getAllTodo();
-  allTodo.forEach(todo => showResult(todo, text));
+  const allTodo = select('.todo span');
+  allTodo.forEach(todo => showMatchedTodo(todo, text));
 };
 
+const matchTask = function(text, task, todo) {
+  const taskId = task.parentElement.parentElement.id;
+  if (task.innerText.includes(text) && text) {
+    getElement(taskId).style['background-color'] = '#2b0f1621';
+  } else {
+    getElement(taskId).style['background-color'] = 'white';
+  }
+  getElement(todo.id).style.display = '';
+};
+
+const showMatchedTask = function(todo, text) {
+  const taskTitle = Array.from(select(`[id="${todo.id}"] p`));
+  const matchedTask = taskTitle.filter(task => task.innerText.includes(text));
+  taskTitle.forEach(task => matchTask(text, task, todo));
+  if (matchedTask.length === 0) {
+    getElement(todo.id).style.display = 'none';
+  }
+};
+
+const searchByTask = function() {
+  const text = event.target.value;
+  const allTodo = select('.todo');
+  allTodo.forEach(todo => showMatchedTask(todo, text));
+};
